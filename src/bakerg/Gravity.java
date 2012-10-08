bdab0f550e1549b85f3a15551cdc380981e0847a
Geoff Baker <geoffreybaker_uk@yahoo.co.uk>
2012-10-08 17:41:44 +0200

    Removed uneccessary

--- src/bakerg/Gravity.java
+++ src/bakerg/Gravity.java
@@ -3,7 +3,6 @@ package bakerg;
 public class Gravity {
 	private static final double G = 6.67E-11; //The universal constant for gravity to 2 significant figures
 	public static double getForce(double m1, double m2, double r) { //m1 = mass1 (kg), m2 = mass2 (kg), r = distance between the centres of gravity of the two objects (m)
-		double F = G * ((m1 * m2) / (r * r));
-		return F; //F = the force in newtons
+		return G * ((m1 * m2) / (r * r));
 	}
 }
